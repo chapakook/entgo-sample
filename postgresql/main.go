b3cdfb298fe33833421e0f701485d5d5aa2ccdb7
chapakook <chapakook96@gmail.com>
2022-03-12 21:13:17 +0900

    postgresql add

--- postgresql/main.go
+++ postgresql/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"entgo.io/ent/entc/integration/edgefield/ent"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	client, err := ent.Open("postgres", "host=<host> port=<port> user=<user> dbname=<database> password=<pass>")
+	checkErr(err)
+	defer client.Close()
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
 }

f5b79f1075b86ed32c786cd68410f85109b93b6a
chapakook <chapakook96@gmail.com>
2022-03-12 21:12:26 +0900

    mysql add

--- mysql/main.go
+++ mysql/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"entgo.io/ent/entc/integration/edgefield/ent"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	client, oErr := ent.Open("mysql", "admin:admin@tcp(127.0.0.1:3306)/pandabank?parseTime=True")
+	checkErr(oErr)
+	defer client.Close()
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
 }

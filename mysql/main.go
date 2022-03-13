d0fcc635ccafa3053793b1550bfc3b08e2b4b55d
chapakook <chapakook96@gmail.com>
2022-03-13 17:11:27 +0900

    mysql update

--- mysql/main.go
+++ mysql/main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"mysql/ent"
 
-	"entgo.io/ent/entc/integration/edgefield/ent"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	client, oErr := ent.Open("mysql", "admin:admin@tcp(127.0.0.1:3306)/pandabank?parseTime=True")
-	checkErr(oErr)
+	client, err := ent.Open("mysql", "admin:admin@tcp(127.0.0.1:3306)/pandabank?parseTime=True")
+	checkErr(err)
 	defer client.Close()
+
+	ctx := context.Background()
+	err = client.Schema.Create(ctx)
+	checkErr(err)
+
+	user := client.User.Create().SetID(1).SetName("bob").SaveX(ctx)
+
+	fmt.Println(user)
 }
 
 func checkErr(err error) {
